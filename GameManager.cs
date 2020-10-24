d49aade54ff1e214393c2f417cc5d492d92acf8b
Jedsada Moon <jedsadamoon@gmaill.com>
2020-10-24 15:25:42 +0700

    add HelloGithub in start

--- GameManager.cs
+++ GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello Github");
     }
 
     // Update is called once per frame
